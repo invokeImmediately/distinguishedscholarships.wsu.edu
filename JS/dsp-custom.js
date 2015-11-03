7533c6bef264ed8350387294b7c00025aae38c8a
Daniel Rieck <danielcrieck@gmail.com>
2015-11-03 14:44:32 -0800

    Commented out jQuery-based removal of the home link on the site root pending team approval

--- JS/dsp-custom.js
+++ JS/dsp-custom.js
@@ -1,70 +1,64 @@
-jQuery.getScript('/documents/2015/06/jquery-qtip-plugin.js');
-jQuery.getScript('/documents/2015/06/jquery-getScript-test.js');
-
-/**********************************************************************************************************************
- JQUERY CYCLE2 PLUGIN
- *********************************************************************************************************************/
-
-/* jQuery Cycle2; version: 2.1.5 build: 20140415 | http://jquery.malsup.com/cycle2/ | Copyright (c) 2014 M. Alsup; Dual licensed: MIT/GPL */
-/*Minified code goes here*/
-    
 /**********************************************************************************************************************
  CUSTOM JQUERY-BASED DYNAMIC CONTENT
  *********************************************************************************************************************/
+(function ($) {
+	$(document).ready(function () {
+			"use strict";
+			/**********************************************************************************************
+			 * Tweak CSS                                                                                 *
+			 **********************************************************************************************/
+			$('div.column h2').each(function () {
+					$(this).prev('hr').addClass('narrow-bottom-margin dark-gray');
+			});
 
-jQuery(document).ready(function () {
-    "use strict";
-    
-    /* Tweak CSS */
-    jQuery('div.column h2').each(function () {
-        jQuery(this).prev('hr').addClass('narrow-bottom-margin dark-gray');
-    });
-    
-    jQuery('.drop-down-toggle').click(function () {
-        jQuery(this).next('.toggledPanel').toggle(500)
-    });
-    
-    jQuery('.read-more-toggle-in-ctrl').click(function () {
-        jQuery(this).toggle(500);
-        jQuery(this).next('.read-more-panel').toggle(500);
-        jQuery(this).next('.read-more-panel').next('.read-more-toggle-out-ctrl').toggle(500);
-    });
-    
-    jQuery('.read-more-toggle-out-ctrl').click(function () {
-        jQuery(this).toggle(500);
-        jQuery(this).next('.read-more-panel').toggle(500);
-        jQuery(this).next('.read-more-panel').next('.read-more-toggle-in-ctrl').toggle(500);
-    });
-    
-    jQuery('.has-tool-tip').each(function () {
-		jQuery(this).qtip({
-            style: 'qtip-dark',
-            content: { text: $(this).next('div')},
-            position: {
-                target: 'mouse', // Track the mouse as the positioning target
-                adjust: { x: 5, y: 15 } // Offset it slightly from under the mouse
-            },
-            show: {
-                effect: function () {
-                    jQuery(this).slideDown(200);
-                }
-            },
-            hide: {
-                effect: function () {
-                    jQuery(this).slideUp(200);
-                }
-            }
-        });
-    });
-        
-    jQuery('.content-flipper').click(function () {
-        jQuery(this).next('.flipped-content-front').toggle(500);
-        jQuery(this).next('.flipped-content-back').fadeToggle(500);
-    });
-    
-    jQuery('.flipped-content-front').click(function () {
-        jQuery(this).next('.flipped-content-front').toggle(500);
-        jQuery(this).next('.flipped-content-back').fadeToggle(500);
-    });
+			/**********************************************************************************************
+			 * Tweak HTML source to work around some quirks of WordPress setup                            *
+			 **********************************************************************************************/
+			var dspSiteURL = window.location.pathname;
+			switch(dspSiteURL) {
+/*				case '/':
+					$('#menu-item-35').remove();
+					$('#spine-sitenav ul li').first().css('border-top', 'none');
+					$('#spine-sitenav').addClass('homeless');
+					break;*/
+				case '/news/':
+					$('div.column.one').first().parent('section').before('<section class="row single gutter pad-top"><div class="column one"><section class="article-header header-newsEvents"><div class="header-content"><h2>News</h2><h3>What We and Our Students Have Accomplished</h3></div></section></div></section>');
+					break;
+			}
+			
+			/**********************************************************************************************
+			 * Implement Dynamic Behaviors of Responsive Elements                                         *
+			 **********************************************************************************************/
+			$('.drop-down-toggle').click(function () {
+					$(this).next('.toggled-panel').toggle(500)
+			});
+			
+			$('.read-more-toggle-in-ctrl').click(function () {
+					$(this).toggle(500);
+					$(this).next('.read-more-panel').toggle(500);
+					$(this).next('.read-more-panel').next('.read-more-toggle-out-ctrl').toggle(500);
+			});
+			
+			$('.read-more-toggle-out-ctrl').click(function () {
+					$(this).toggle(500);
+					$(this).next('.read-more-panel').toggle(500);
+					$(this).next('.read-more-panel').next('.read-more-toggle-in-ctrl').toggle(500);
+			});
+			
+			$('.content-flipper').click(function () {
+					$(this).next('.flipped-content-front').toggle(500);
+					$(this).next('.flipped-content-front').next('.flipped-content-back').fadeToggle(500);
+			});
+			
+			$('.flipped-content-front').click(function () {
+					$(this).toggle(500);
+					$(this).next('.flipped-content-back').fadeToggle(500);
+			});
+			
+			/**********************************************************************************************
+			 * Tweak University Communication's use of the Cycle2 plugin                                  *
+			 **********************************************************************************************/
+			$('div.cycle-slideshow').attr('data-cycle-pause-on-hover','true');
 
-});
+	});
+})(jQuery);
