96a3f9ca7a3d691293525691bcceacd1e4034106
Daniel Rieck <danielcrieck@gmail.com>
2015-11-05 12:27:58 -0800

    Added HTML for Goldwater Award info page, updated the JavaScript plugin loading code, and added new events to the calendar page

--- JS/wp-custom-javascript-source.dsp.js
+++ JS/wp-custom-javascript-source.dsp.js
@@ -1,7 +1,8 @@
+// See [https://github.com/invokeImmediately/distinguishedscholarships.wsu.edu] for repository of source code
 (function($) {
-    $.ajax({ url: '/wp-content/uploads/sites/50/2015/10/jQuery-dsp-custom-v1-022.js', dataType: 'script', cache: true });
+    $.ajax({ url: '/wp-content/uploads/sites/50/2015/11/dsp-custom.js', dataType: 'script', cache: true });
     $.ajax({ url: '/wp-content/uploads/sites/50/2015/10/jQuery.textResize-v04.js', dataType: 'script', cache: true });
-    ($('.has-tool-tip').length !== 0) ? $.ajax({ url: '/wp-content/uploads/sites/50/2015/10/jQuery.qTip_.js', dataType: 'script', cache: true }) : 0;
+    ($('.has-tool-tip').length !== 0) ? $.ajax({ url: '/wp-content/uploads/sites/50/2015/11/jQuery.qTip_.js', dataType: 'script', cache: true }) : 0;
     ($('.page-covering-notice-js').length !== 0) ? $.ajax({ url: '/wp-content/uploads/sites/50/2015/10/jQuery.cookieObjs.js', dataType: 'script', cache: true }) : 0;
     ($('a.media').length !== 0) ? $.ajax({ url: '/wp-content/uploads/sites/50/2015/10/jquery.media_.js', dataType: 'script', cache: true }) : 0;
     ($('table.calendar').length !== 0) ? $.ajax({ url: '/wp-content/uploads/sites/50/2015/10/jQuery.animatedCalendar.js', dataType: 'script', cache: true }) : 0;
