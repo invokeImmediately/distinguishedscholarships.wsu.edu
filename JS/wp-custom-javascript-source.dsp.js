ba74665fcc7f82e6d55338352bdc36c05c339c15
Daniel Rieck <danielcrieck@gmail.com>
2016-01-04 11:34:43 -0800

    Rebuilt production files to incorporate recent changes to source code

--- JS/wp-custom-javascript-source.dsp.js
+++ JS/wp-custom-javascript-source.dsp.js
@@ -5,13 +5,6 @@
 (function ($) {
 	$(document).ready(function () {
 			/**********************************************************************************************
-			 * Tweak CSS                                                                                 *
-			 **********************************************************************************************/
-			$('div.column h2').each(function () {
-					$(this).prev('hr').addClass('narrow-bottom-margin dark-gray');
-			});
-
-			/**********************************************************************************************
 			 * Tweak HTML source to work around some quirks of WordPress setup                            *
 			 **********************************************************************************************/
 			var dspSiteURL = window.location.pathname;
@@ -25,43 +18,87 @@
 					$('div.column.one').first().parent('section').before('<section class="row single gutter pad-top"><div class="column one"><section class="article-header header-newsEvents"><div class="header-content"><h2>News</h2><h3>What We and Our Students Have Accomplished</h3></div></section></div></section>');
 					break;
 			}
-			
+	});
+})(jQuery);/**********************************************************************************************************************
+ CUSTOM JQUERY-BASED DYNAMIC CONTENT
+ *********************************************************************************************************************/
+(function ($) {
+    "use strict";
+	$(document).ready(function () {
 			/**********************************************************************************************
-			 * Implement Dynamic Behaviors of Responsive Elements                                         *
+			 * As desired, tweak the CSS of the previous sibling of certain selected elements in the DOM  *
+			 **********************************************************************************************/
+			$('div.column > h2').each(function () {
+					$(this).prev('hr').addClass('narrow-bottom-margin dark-gray thicker');
+			});
+			$('div.column > h3').each(function () {
+					$(this).prev('hr:not(.subSection)').addClass('narrow-bottom-margin crimson');
+			});
+
+			/**********************************************************************************************
+			 * Set column heights on fluid-width containters                                              *
+			 **********************************************************************************************/
+			$('.large-format-friendly > div.column.two').each(function () {
+					var $this = $(this);
+                    $this.height($this.prev('div.column.one').height());
+			});
+            $(window).resize(function () {
+                $('.large-format-friendly > div.column.two').each(function () {
+					var $this = $(this);
+                    var $thisPrev = $this.prev('div.column.one');
+                    if($this.height() != $thisPrev.height() ) {
+                        $this.height($thisPrev.height());
+                    }
+                });
+            });
+            
+			/**********************************************************************************************
+			 * Implement dynamic behaviors of interactive elements                                        *
 			 **********************************************************************************************/
 			$('.drop-down-toggle').click(function () {
-					$(this).next('.toggled-panel').toggle(500)
+                var $this = $(this);
+                $this.toggleClass('activated');
+                $this.next('.toggled-panel').toggle(500)
 			});
-			
 			$('.read-more-toggle-in-ctrl').click(function () {
-					$(this).toggle(500);
-					$(this).next('.read-more-panel').toggle(500);
-					$(this).next('.read-more-panel').next('.read-more-toggle-out-ctrl').toggle(500);
+                var $this = $(this);
+                $this.toggle(500);
+                $this.next('.read-more-panel').toggle(500);
+                $this.next('.read-more-panel').next('.read-more-toggle-out-ctrl').toggle(500);
 			});
-			
 			$('.read-more-toggle-out-ctrl').click(function () {
-					$(this).toggle(500);
-					$(this).next('.read-more-panel').toggle(500);
-					$(this).next('.read-more-panel').next('.read-more-toggle-in-ctrl').toggle(500);
+                var $this = $(this);
+                $this.toggle(500);
+                $this.next('.read-more-panel').toggle(500);
+                $this.next('.read-more-panel').next('.read-more-toggle-in-ctrl').toggle(500);
 			});
-			
 			$('.content-flipper').click(function () {
-					$(this).next('.flipped-content-front').toggle(500);
-					$(this).next('.flipped-content-front').next('.flipped-content-back').fadeToggle(500);
+                var $this = $(this);
+                $this.next('.flipped-content-front').toggle(500);
+                $this.next('.flipped-content-front').next('.flipped-content-back').fadeToggle(500);
 			});
-			
 			$('.flipped-content-front').click(function () {
-					$(this).toggle(500);
-					$(this).next('.flipped-content-back').fadeToggle(500);
+                var $this = $(this);
+                $this.toggle(500);
+                $this.next('.flipped-content-back').fadeToggle(500);
 			});
-			
-			/**********************************************************************************************
-			 * Tweak University Communication's use of the Cycle2 plugin                                  *
-			 **********************************************************************************************/
-			$('div.cycle-slideshow').attr('data-cycle-pause-on-hover','true');
-
+            $('#welcome-message').delay(1000).fadeOut(500, function () {
+                $('#post-welcome-message').fadeIn(500);
+            });
+            $("dl.toggled dt").click(function() {
+                var $this = $(this);
+                $this.toggleClass('activated');
+                $this.next("dd").slideToggle(400, function () {
+                    var $parent = $this.parents('.large-format-friendly > div.column.one');
+                    var $prntNxt = $parent.next('div.column.two');
+                    $prntNxt.animate({height: $parent.css('height')}, 100);
+                });
+            });
+            $("dd").hide();           
+            
 	});
-})(jQuery);(function ($) {
+})(jQuery);
+(function ($) {
     /*	SCRIPT DESCRIPTION: Obtains the current dait and modifies the "Today's Date" hyperlink above as appropriate. The calendar
         below is specifically coded to accomodate navigation to each work-week calendar day within the main WSU semesters.
     AUTHORED BY: DC Rieck | Designed for further Use with: Notepad++
