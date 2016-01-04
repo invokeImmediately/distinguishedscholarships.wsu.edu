1368d53eaa4c61473e0a6cf392e1ef63c458f707
Daniel Rieck <danielcrieck@gmail.com>
2016-01-04 11:34:24 -0800

    Added a node.js file for building production versions of JS source code

---
+++ JS/build-production-file.js
@@ -0,0 +1,15 @@
+/* NODE.JS - Build Production JavaScript File */
+var concat = require('../../../../AppData/Roaming/npm/node_modules/concat-files');
+concat([
+ './dsp-custom.js',
+ '../WSU-UE---JS/jQuery.oue-custom.js',
+ '../WSU-UE---JS/jQuery.animatedCalendar.js',
+ '../WSU-UE---JS/jQuery.autoScrollingImages.js',
+ '../WSU-UE---JS/jQuery.cookieObjs.js',
+ '../WSU-UE---JS/jQuery.cycle2.js',
+ '../WSU-UE---JS/jquery.media.js',
+ '../WSU-UE---JS/jQuery.qTip.js',
+ '../WSU-UE---JS/jQuery.textResize.js'
+ ], './wp-custom-javascript-source.dsp.js', function() {
+    console.log('Concatenation complete.');     
+ });
