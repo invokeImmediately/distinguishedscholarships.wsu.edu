5f0643ce6241ea4fb27e8fbdf21ec947c173028e
Daniel Rieck <danielcrieck@gmail.com>
2016-01-05 14:11:33 -0800

    Added backup of old AJAX-based approach for loading code to repository

---
+++ JS/wp-custom-js-source.ajax.dsp.js
@@ -0,0 +1,10 @@
+// See [https://github.com/invokeImmediately/distinguishedscholarships.wsu.edu] for repository of source code
+(function($) {
+    $.ajax({ url: '/wp-content/uploads/sites/50/2015/11/dsp-custom.js', dataType: 'script', cache: true });
+    $.ajax({ url: '/wp-content/uploads/sites/50/2015/10/jQuery.textResize-v04.js', dataType: 'script', cache: true });
+    ($('.has-tool-tip').length !== 0) ? $.ajax({ url: '/wp-content/uploads/sites/50/2015/11/jQuery.qTip_.js', dataType: 'script', cache: true }) : 0;
+    ($('.page-covering-notice-js').length !== 0) ? $.ajax({ url: '/wp-content/uploads/sites/50/2015/10/jQuery.cookieObjs.js', dataType: 'script', cache: true }) : 0;
+    ($('a.media').length !== 0) ? $.ajax({ url: '/wp-content/uploads/sites/50/2015/10/jquery.media_.js', dataType: 'script', cache: true }) : 0;
+    ($('table.calendar').length !== 0) ? $.ajax({ url: '/wp-content/uploads/sites/50/2015/10/jQuery.animatedCalendar.js', dataType: 'script', cache: true }) : 0;
+    ($('.cycle2-slideshow').length !== 0) ? $.ajax({ url: '/wp-content/uploads/sites/50/2015/10/jQuery.cycle2_.js', dataType: 'script', cache: true }) : 0;
+})(jQuery);
