f2e91217f6e96d1357d099a42f3492ea9d39d77d
Daniel Rieck <danielcrieck@gmail.com>
2016-06-09 12:50:06 -0700

    Updating submodule
    Updated master JS submodule to incorporate recent changes in project source code

--- JS/dsp-custom.js
+++ JS/dsp-custom.js
@@ -4,19 +4,37 @@
  *********************************************************************************************************************/
 (function ($) {
 	$(document).ready(function () {
-			/**********************************************************************************************
-			 * Tweak HTML source to work around some quirks of WordPress setup                            *
-			 **********************************************************************************************/
-			var dspSiteURL = window.location.pathname;
-			switch(dspSiteURL) {
+        /**********************************************************************************************
+         * Tweak HTML source to work around some quirks of WordPress setup                            *
+         **********************************************************************************************/
+        var siteURL = window.location.pathname;
+        switch(siteURL) {
 /*				case '/':
-					$('#menu-item-35').remove();
-					$('#spine-sitenav ul li').first().css('border-top', 'none');
-					$('#spine-sitenav').addClass('homeless');
-					break;*/
-				case '/news/':
-					$('div.column.one').first().parent('section').before('<section class="row single gutter pad-top"><div class="column one"><section class="article-header header-newsEvents"><div class="header-content"><h2>News</h2><h3>What We and Our Students Have Accomplished</h3></div></section></div></section>');
-					break;
-			}
+                $('#menu-item-35').remove();
+                $('#spine-sitenav ul li').first().css('border-top', 'none');
+                $('#spine-sitenav').addClass('homeless');
+                break;*/
+            case '/news/':
+                $('div.column.one').first().parent('section').before('<section class="row single gutter pad-top"><div class="column one"><section class="article-header header-newsEvents"><div class="header-content"><h2>News</h2><h3>What We and Our Students Have Accomplished</h3></div></section></div></section>');
+                break;
+        }
+        InitWsuIdInputs(".gf-wsu-id-input");
 	});
+    
+    function InitWsuIdInputs(slctrInputs) {
+        var $wsuIdInputs = $(slctrInputs).find("input[type='text']");
+        $wsuIdInputs.change(function () {
+            var $this = $(this);
+            var regExMask = /[^0-9]+/g;
+            var inputText = $this.val();
+            $this.val(inputText.replace(regExMask, ""));
+        }).blur(function () {
+            var $this = $(this);
+            var regExFinalPttrn = /(?:^[0-9]{8}$)|(?:^0[0-9]{8}$)/;
+            var inputText = $this.val();
+            if (regExFinalPttrn.exec(inputText) == null) {
+                $this.val("");
+            }
+        });
+    }
 })(jQuery);
