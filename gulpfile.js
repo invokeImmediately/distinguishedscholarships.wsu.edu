2c508690b87b0f4a9f6b09d8fa7f59bd67032394
Daniel Rieck <danielcrieck@gmail.com>
2017-05-27 21:23:34 -0700

    Updating gulp automation process with new steps
    Changing the file extension before final write, inserting a documentation comment at beginning of file

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp'),
 	gcmq = require('gulp-group-css-media-queries'),
 	insertLines = require('gulp-insert-lines'),
-  cleanCss = require('gulp-clean-css'),
-  dest = require('gulp-dest');
+	cleanCss = require('gulp-clean-css'),
+	insert = require('gulp-insert'),
+	extName = require('gulp-extname');
 
 gulp.task('default', function () {
 	gulp.src('./CSS/dsp-custom.css')
@@ -10,9 +11,10 @@ gulp.task('default', function () {
 		.pipe(insertLines({
 			'before': /^@media/,
 			'lineBefore': '/*! ╔═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗\r\n*   ║ MEDIA QUERIES ####################################################################################################### ║\r\n*   ╚═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝\r\n*/',
-      'stopAfterFirstMatch': true
+			'stopAfterFirstMatch': true
 		}))
-    .pipe(cleanCss())
-    .pipe(dest('CSS/:name.min.css'))
-		.pipe(gulp.dest('.'));
+		.pipe(cleanCss())
+		.pipe(insert.prepend('/* Built with the LESS CSS preprocessor [http://lesscss.org/]. Please see [https://github.com/invokeImmediately/distinguishedscholarships.wsu.edu] for a repository of source code. */\r\n'))
+		.pipe(extName('.min.css'))
+		.pipe(gulp.dest('CSS'));
 });
