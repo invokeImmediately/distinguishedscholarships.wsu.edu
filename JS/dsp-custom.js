eae913d568789b12df58f5f53dc5924e5b442850
Daniel Rieck <danielcrieck@gmail.com>
2017-11-01 15:45:03 -0700

    Cleaning up code according to jQuery style guide

--- JS/dsp-custom.js
+++ JS/dsp-custom.js
@@ -1,24 +1,19 @@
-// See [https://github.com/invokeImmediately/distinguishedscholarships.wsu.edu] for repository of source code
-/**********************************************************************************************************************
- CUSTOM JQUERY-BASED DYNAMIC CONTENT
- *********************************************************************************************************************/
-(function ($) {
-    "use strict";
+/*!
+ * dsp-custom.js: Site-specific JS for the WSU Distinguished Scholarships website.
+ * Author:  Daniel Rieck ( danielcrieck@gmail.com ) [https://github.com/invokeImmediately]
+ */
+( function ( $ ) {
+"use strict";
     
-	$(document).ready(function () {
-        /**********************************************************************************************
-         * Tweak HTML source to work around some quirks of WordPress setup                            *
-         **********************************************************************************************/
-        var siteURL = window.location.pathname;
-        switch(siteURL) {
-/*				case '/':
-                $('#menu-item-35').remove();
-                $('#spine-sitenav ul li').first().css('border-top', 'none');
-                $('#spine-sitenav').addClass('homeless');
-                break;*/
-            case '/news/':
-                $('div.column.one').first().parent('section').before('<section class="row single gutter pad-top"><div class="column one"><section class="article-header header-newsEvents"><div class="header-content"><h2>News</h2><h3>What We and Our Students Have Accomplished</h3></div></section></div></section>');
-                break;
-        }
-	});    
-})(jQuery);
+$( function () {
+	
+	// Tweak HTML source to work around some quirks of WordPress setup
+	var siteURL = window.location.pathname;
+	switch( siteURL ) {
+		case '/news/':
+			$( 'div.column.one' ).first().parent( 'section' ).before( '<section class="row single gutter pad-top"><div class="column one"><section class="article-header header-newsEvents"><div class="header-content"><h2>News</h2><h3>What We and Our Students Have Accomplished</h3></div></section></div></section>' );
+			break;
+	}
+} );
+
+} )( jQuery );
