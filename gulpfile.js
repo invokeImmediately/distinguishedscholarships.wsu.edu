0dbcf9a43b70f480a99a744cc1bc130900477ca4
Daniel Rieck <danielcrieck@gmail.com>
2018-10-10 22:27:29 -0700

    Refactor file to rely on dependency from WSU-UE---JS
    Refactor file to utilize new node module in WSU-UE---JS that centralizes code common to all gulp-mediated, OUE-wide JS build processes.

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,24 @@
 */
 
 // Gulp task dependencies
-var cleanCss = require( 'gulp-clean-css' );
-var concat = require( 'gulp-concat' );
-var extName = require( 'gulp-extname' );
-var gcmq = require( 'gulp-group-css-media-queries' );
-var gulp = require( 'gulp' );
-var insert = require( 'gulp-insert' );
-var insertLines = require( 'gulp-insert-lines' );
-var lessc = require( 'gulp-less' );
-var replace = require( 'gulp-replace' );
-var uglifyJs = require( 'gulp-uglify' );
-var pump = require( 'pump' );
+var gulpBuilder = require( './WSU-UE---JS/gulpBuilder.js' );
 
 /* -------------------------------------------------------------------------------------------------
 ** Function declarations
 */
 
+/**
+ * 
+ * @return {Object}		Collection of objects
+ */
+
 function getCssBuildSettings() {
-	return {
-		commentRemovalNeedle: /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm,
-		dependenciesPath: './WSU-UE---CSS/',
-		destFolder: './CSS/',
-		fontImportStr: '@import url(\'https://fonts.googleapis.com/css?family=Roboto+Mono:400,700|R\
-oboto+Condensed:400,700|Roboto+Slab|PT+Serif\');\r\n',
-		insertingMediaQuerySectionHeader: {
+	var commentRemovalNeedle = /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm;
+	var dependenciesPath = './WSU-UE---CSS/';
+	var destFolder = './CSS/';
+	var fontImportStr = '@import url(\'https://fonts.googleapis.com/css?family=Roboto+Mono:400,700|\
+Roboto+Condensed:400,700|Roboto+Slab|PT+Serif\');\r\n';
+	var insertingMediaQuerySectionHeader = {
 			'before': /^@media/,
 			'lineBefore': '/*! ╔═══════════════════════════════════════════════════════════════════\
 ════════════════════════════════════════════════════╗\r\n*   ║ MEDIA QUERIES ######################\
@@ -36,13 +30,15 @@ oboto+Condensed:400,700|Roboto+Slab|PT+Serif\');\r\n',
 ═══════════════════════════════════════════════════════════════════════════════════════════════════\
 ═════════════╝\r\n*/',
 			'stopAfterFirstMatch': true
-		},
-		minCssFileExtension: '.min.css',
-		minCssFileHeaderStr: '/*! Built with the Less CSS preprocessor [http://lesscss.org/]. Pleas\
-e see [https://github.com/invokeImmediately/distinguishedscholarships.wsu.edu] for a repository of \
-source code. */\r\n',
-		sourceFile: './CSS/dsp-custom.less'
-	};
+		};
+	var minCssFileExtension = '.min.css';
+	var minCssFileHeaderStr = '/*! Built with the Less CSS preprocessor [http://lesscss.org/]. Plea\
+se see [https://github.com/invokeImmediately/distinguishedscholarships.wsu.edu] for a repository of\
+ source code. */\r\n';
+ 	var sourceFile = './CSS/dsp-custom.less';
+
+	return new gulpBuilder.CssBuildSettings(commentRemovalNeedle, destFolder, fontImportStr,
+		insertingMediaQuerySectionHeader, minCssFileExtension, minCssFileHeaderStr, sourceFile);
 }
 
 function getJsBuildSettings() {
@@ -69,64 +65,13 @@ function getJsBuildSettings() {
 		compiledJsFileName: 'dsp-custom-build.js',
 		destFolder: './JS/',
 		minJsFileExtension: '.min.js',
-		replaceCallback: fixFileHeaderComments
+		replaceCallback: gulpBuilder.fixFileHeaderComments
 	};
-}
-
-function fixFileHeaderComments ( match, p1, offset, string ) {
-	var replacementStr = match;
-	if ( offset == 0 ) {
-		replacementStr = '/*!';
-	}
-	return replacementStr;
-}
-
-function setUpCssBuildTask( settings ) {
-	gulp.task( 'buildMinCss', function ( callBack ) {
-		pump( [
-				gulp.src( settings.sourceFile ),
-				lessc( {
-					paths: [settings.dependenciesPath]
-				} ),
-				replace( settings.commentRemovalNeedle, '' ),
-				insert.prepend( settings.fontImportStr ),
-				insert.prepend( settings.minCssFileHeaderStr ),
-				gulp.dest( settings.destFolder ),
-				gcmq(),
-				insertLines( settings.insertingMediaQuerySectionHeader ),
-				cleanCss(),
-				extName( settings.minCssFileExtension ),
-				gulp.dest( settings.destFolder )
-			],
-			callBack
-		);
-	} );
-}
-
-function setUpJsBuildTask( settings ) {
-	gulp.task( 'buildMinJs', function ( callBack ) {
-		pump( [
-				gulp.src( settings.buildDependenciesList ),
-				replace( settings.commentNeedle, settings.replaceCallback ),
-				concat( settings.compiledJsFileName ),
-				gulp.dest( settings.destFolder ),
-				uglifyJs( {
-					output: {
-						comments: /^!/
-					},
-					toplevel: true,
-				} ),
-				extName( settings.minJsFileExtension ),
-				gulp.dest( settings.destFolder )
-			],
-			callBack
-		);
-	} );
 }
 
 /* -------------------------------------------------------------------------------------------------
 ** Main execution sequence
 */
 
-setUpCssBuildTask( getCssBuildSettings() );
-setUpJsBuildTask( getJsBuildSettings() );
+gulpBuilder.setUpCssBuildTask( getCssBuildSettings() );
+gulpBuilder.setUpJsBuildTask( getJsBuildSettings() );
