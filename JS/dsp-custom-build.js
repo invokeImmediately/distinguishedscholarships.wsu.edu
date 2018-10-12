92757dc2e725a505eaf5e3a466ba44270c3e21ae
Daniel Rieck <danielcrieck@gmail.com>
2018-10-12 16:36:26 -0700

    Updating custom JS build w/ site-specific source changes
    Rebuilding custom CSS production files to incorporate recent changes to site-specific build dependency(ies); please see the next commit for more details.

--- JS/dsp-custom-build.js
+++ JS/dsp-custom-build.js
@@ -4,6 +4,10 @@
  * @author - Daniel Rieck ( danielcrieck@gmail.com ) [https://github.com/invokeImmediately]
  */
 
+/* -------------------------------------------------------------------------------------------------
+** §1: Main execution
+*/
+
 /**
  * IIFE for main execution.
  */
@@ -12,7 +16,7 @@
 "use strict";
 
 /**
- * IIFE for executing statements after the DOM has loaded.
+ * jQuery call for executing statements after the DOM has loaded.
  */
 $( function () {
 	var htmlNewsHeader = '<section class="row single gutter pad-top"><div class="column one"><secti\
@@ -23,8 +27,67 @@ d Our Students Have Accomplished</h3></div></section></div></section>';
 	addPageHeaderOnNewsPages( htmlNewsHeader );
 } );
 
+/**
+ * jQuery call for executing statements after the window has finished loading.
+ */
+$( window ).on( 'load', function () {
+	var selectors = {};
+	selectors.galleryWall = '.page-header__gallery-wall-panorama';
+	selectors.galleryWallContainer = '.page-header__gallery-wall-wrapper';
+	animateGalleryWallHeader( selectors.galleryWall, selectors.galleryWallContainer, 132, 2 );
+} );
+
 /* -------------------------------------------------------------------------------------------------
-** Function Declarations
+** §2: Class declarations
+*/
+
+/**
+ * Create a new instance of an animated gallery wall object, which causes gallery wall headers to
+ * pan back and forth.
+ * 
+ * @class
+ *
+ * @param {string} headerSlctr - jQuery selector for the header objects that will be animated.
+ */
+function AnimatedGalleryWall( headerSlctr, containerSlctr, speed, numPans ) {
+
+	initializeHeaders();
+
+	function initializeHeaders() {
+		var $headers;
+		var $thisHeader;
+
+		$headers = $( headerSlctr );
+		if ( $headers.length > 0 ) {
+			$headers.each( function() {
+				$thisHeader = $( this );
+				panHeader($thisHeader);
+			} );
+		}		
+	}
+
+	function panHeader( $header ) {
+		var $container
+		var containerWidth;
+		var headerWidth;
+		var idx;
+		var newLeftPos
+		var panDuration;
+
+		$container = $header.parent( containerSlctr );
+		headerWidth = $header.width();
+		containerWidth = $container.width();
+		newLeftPos = -1 * headerWidth + containerWidth;
+		panDuration = (headerWidth - containerWidth) / speed * 1000;
+		for (idx = 0; idx < numPans; idx++) {
+			$header.animate( { left: newLeftPos }, panDuration ).animate( { left: 0 },
+				panDuration );
+		}
+	}
+}
+
+/* -------------------------------------------------------------------------------------------------
+** §3: Function Declarations
 */
 
 /**
@@ -58,10 +121,20 @@ function aPHONP_addHeaderViaLocation( htmlNewsHeader ) {
  */
 function aPHONP_addHeaderViaClassUtilization( htmlNewsHeader ) {
 	var $body = $( 'body' ).first();
-	var $div;
 	if ( $body.hasClass( 'single-post' ) ) {
-		$body.find( '.column.one' ).first().parent( '.row' ).before( htmlNewsHeader );		
+		$body.find( '.column.one' ).first().parent( '.row' ).before( htmlNewsHeader );
 	}
+}
+
+/**
+ * Set up a horizontal panning animation on the header of gallery wall pages.
+ *
+ * @param {String} headerSlctr - Selector for the header graphic that will be panned.
+ * @param {String} containerSlctr - Selector for the container of the header graphic. This function
+ *     assumes it has its overflow CSS property set to hidden.
+ */
+function animateGalleryWallHeader( headerSlctr, containerSlctr, duration, numPans ) {
+	var animObj = new AnimatedGalleryWall( headerSlctr, containerSlctr, duration, numPans );
 }
 
 } )( jQuery );
