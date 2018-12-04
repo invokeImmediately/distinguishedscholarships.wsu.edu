8450098d59df56bda8c02fddbb3d3cd0acad347d
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2018-12-04 13:50:50 -0800

    Remove masonry and images loaded plugins as dependencies
    These plugins are now being provided through import options available in WSUWP's Custom JavaScript Editor.

--- gulpfile.js
+++ gulpfile.js
@@ -63,8 +63,6 @@ function getJsBuildSettings() {
 			'../qTip2/dist/jquery.qtip.min.js',
 			'./WSU-UE---JS/jQuery.qTip.js',
 			'./WSU-UE---JS/jQuery.textResize.js',
-			'../imagesloaded/imagesloaded.pkgd.min.js',
-			'../masonry/dist/masonry.pkgd.min.js',
 			'./WSU-UE---JS/jQuery.masonry-custom.js',
 			'./JS/dsp-custom.js'
 		],
