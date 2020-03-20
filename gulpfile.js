1ee246f341881dfd2e13fab9259c277d7bf6dfe4
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2020-03-19 17:35:03 -0700

    Update dev dependencies for and builds of custom CSS code
    In CSS dev dependency gulpfile.js, update the font import command that is inserted at the beginning of the custom CSS build process. Then, update the file header comment in dsp-custom.less. Next, update the submodule containing universal dev dependencies used to build custom CSS code in every DAESA website development project. Finally, rebuild the files containing custom CSS code that is ready to be deployed on the website associated with this project.

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,22 @@
  * SUMMARY: Gulp automation task definition file for setting up tasks that build CSS and JS
  * files for use on the WSUWP website of the WSU Distinguished Scholarships program.
  *
- * DESCRIPTION: This gulp automation task definition file is designed for use ont he following
+ * DESCRIPTION: This gulp automation task definition file is designed for use on the following
  *   project that is maintained on GitHub:
  *   https://github.com/invokeImmediately/distinguishedscholarships.wsu.edu
  *
  * AUTHOR: Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
  *
- * LICENSE: ISC - Copyright (c) 2019 Daniel C. Rieck.
+ * LICENSE: ISC - Copyright (c) 2020 Daniel C. Rieck.
  *
  *   Permission to use, copy, modify, and/or distribute this software for any purpose with or
  *   without fee is hereby granted, provided that the above copyright notice and this permission
  *   notice appear in all copies.
  *
- *   THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
- *   SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
- *   DANIEL RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
- *   DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ *   THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL C. RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ *   THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT
+ *   SHALL DANIEL C. RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR
+ *   ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
  *   CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  *   PERFORMANCE OF THIS SOFTWARE.
  **************************************************************************************************/
@@ -30,8 +30,8 @@
 // §1: Gulp task dependencies..................................................................42
 // §2: Specificiation of build settings .......................................................47
 //   §2.1: getCssBuildSettings()...............................................................50
-//   §2.2: getJsBuildSettings()................................................................99
-// §3: Entry point: Set up of build taks......................................................134
+//   §2.2: getJsBuildSettings()...............................................................103
+// §3: Entry point: Set up of build taks......................................................138
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ( function() {
@@ -58,32 +58,36 @@ function getCssBuildSettings() {
 	var commentRemovalNeedle = /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm;
 	var dependenciesPath = './WSU-UE---CSS/';
 	var destFolder = './CSS/';
-	var fontImportStr = '@import url(\'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,4\
-00,400i,700,700i|Roboto+Condensed:400,400i,700,700i|PT+Serif:400,400i,700,700i|Roboto+Mono:400,400i\
-,700,700i&display=swap\');\r\n';
+	var fontImportStr = '@import url(\'https://fonts.googleapis.com/css?family=Open+Sans:300,300i' +
+		',400,400i,600,600i,700,700i|Roboto+Condensed:400,400i,700,700i|PT+Serif:400,400i,700,700' +
+		'i|Roboto+Mono:400,400i,700,700i&display=swap\');\r\n';
 	var insertingMediaQuerySectionHeader = {
 			'before': /^@media/,
-			'lineBefore': '/*! ====================================================================\
-============================\r\n*** Media queries section\r\n*** ==================================\
-==============================================================\r\n***   SUMMARY: Media queries buil\
-t from precompiled CSS written in the Less language extension of\r\n***    CSS. Queries in this sec\
-tion are a combination of those designed for use on DAESA websites***\r\n    and those intended spe\
-cifically for use on the Distinguished Scholarships program website.\r\n***\r\n***   DESCRIPTION: F\
-ully documented, precompiled source code from which this section of the custom\r\n***    stylesheet\
- was built is developed and maintained on the following two GitHub projects:\r\n***    https://gith\
-ub.com/invokeImmediately/WSU-UE---CSS/\r\n***    https://github.com/invokeImmediately/distinguished\
-scholarships.wsu.edu/\r\n***   AUTHOR: Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invo\
-keImmediately)\r\n***\r\n***   LICENSE: ISC - Copyright (c) 2019 Daniel C. Rieck.\r\n***\r\n***    \
- Permission to use, copy, modify, and/or distribute this software for any purpose with or\r\n***   \
-  without fee is hereby granted, provided that the above copyright notice and this permission\r\n**\
-*     notice appear in all copies.\r\n***\r\n***     THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL RI\
-ECK DISCLAIMS ALL WARRANTIES WITH REGARD TO\r\n***     THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTI\
-ES OF MERCHANTABILITY AND FITNESS. IN NO EVENT\r\n***     SHALL DANIEL RIECK BE LIABLE FOR ANY SPEC\
-IAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR\r\n***     ANY DAMAGES WHATSOEVER RESULTING FROM\
- LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF\r\n***     CONTRACT, NEGLIGENCE OR OTHER TOR\
-TIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE\r\n***     OR PERFORMANCE OF THIS SOFTWA\
-RE.\r\n*** ========================================================================================\
-========\r\n**/',
+			'lineBefore': '/*! ==================================================================' +
+				'==============================\r\n*** Media queries section\r\n*** =============' +
+				'================================================================================' +
+				'===\r\n***   SUMMARY: Media queries built from precompiled CSS written in the Le' +
+				'ss language extension of\r\n***    CSS. Queries in this section are a combinatio' +
+				'n of those designed for use on DAESA websites***\r\n    and those intended speci' +
+				'fically for use on the Distinguished Scholarships program website.\r\n***\r\n***' +
+				'   DESCRIPTION: Fully documented, precompiled source code from which this sectio' +
+				'n of the custom\r\n***    stylesheet was built is developed and maintained on th' +
+				'e following two GitHub projects:\r\n***    https://github.com/invokeImmediately/' +
+				'WSU-UE---CSS/\r\n***    https://github.com/invokeImmediately/distinguishedschola' +
+				'rships.wsu.edu/\r\n***   AUTHOR: Daniel Rieck [daniel.rieck@wsu.edu] (https://gi' +
+				'thub.com/invokeImmediately)\r\n***\r\n***   LICENSE: ISC - Copyright (c) 2020 Da' +
+				'niel C. Rieck.\r\n***\r\n***     Permission to use, copy, modify, and/or distrib' +
+				'ute this software for any purpose with or\r\n***     without fee is hereby grant' +
+				'ed, provided that the above copyright notice and this permission\r\n***     noti' +
+				'ce appear in all copies.\r\n***\r\n***     THE SOFTWARE IS PROVIDED "AS IS" AND ' +
+				'DANIEL C. RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO\r\n***     THIS SOFTWARE' +
+				' INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT\r' +
+				'\n***     SHALL DANIEL C. RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR ' +
+				'CONSEQUENTIAL DAMAGES OR\r\n***     ANY DAMAGES WHATSOEVER RESULTING FROM LOSS O' +
+				'F USE, DATA OR PROFITS, WHETHER IN AN ACTION OF\r\n***     CONTRACT, NEGLIGENCE ' +
+				'OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE\r\n***   ' +
+				'  OR PERFORMANCE OF THIS SOFTWARE.\r\n*** ======================================' +
+				'==========================================================\r\n**/',
 			'stopAfterFirstMatch': true
 		};
 	var minCssFileExtension = '.min.css';
